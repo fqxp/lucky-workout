d3c7335952fd01e51bdecac1726429405455448b
Frank <frank@fqxp.de>
2020-04-26 23:37:55 +0200

    Hopefully fix URI param

--- static/app.js
+++ static/app.js
@@ -12,7 +12,7 @@ function createIFrame(videoId) {
 
   iframeEl.setAttribute('id', 'player');
   iframeEl.setAttribute('type', 'text/html');
-  iframeEl.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=0&origin=' + document.location.origin);
+  iframeEl.setAttribute('src', 'https://www.youtube.com/embed/' + videoId + '?autoplay=0&origin=' + encodeURIComponent(document.location.origin));
   iframeEl.setAttribute('allowfullscreen', '1');
   iframeEl.setAttribute('controls', '0');
   iframeEl.setAttribute('frameborder', '0');
